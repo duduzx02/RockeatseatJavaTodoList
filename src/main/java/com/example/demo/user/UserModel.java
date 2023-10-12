22088f534a6c1b5cd087d09b2df42218518b52c9
duduzx02 <duduzx02@gmail.com>
2023-10-12 03:24:32 -0300

    Criando tabela @Entity, criando id com UUID e criando um marcador de tempo com o localDateTime

--- src/main/java/com/example/demo/user/UserModel.java
+++ src/main/java/com/example/demo/user/UserModel.java
@@ -1,6 +1,13 @@
 package com.example.demo.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 /*
 * @Getter - só getters
@@ -8,8 +15,17 @@ import lombok.Data;
 * */
 
 @Data // Getters e Setters
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
