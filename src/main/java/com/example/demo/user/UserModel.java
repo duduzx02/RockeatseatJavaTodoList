d6a02fe480ab086d14968da4354157aabe21b78e
duduzx02 <duduzx02@gmail.com>
2023-10-12 01:21:36 -0300

    Lombok

--- src/main/java/com/example/demo/user/UserModel.java
+++ src/main/java/com/example/demo/user/UserModel.java
@@ -1,31 +1,15 @@
 package com.example.demo.user;
 
+import lombok.Data;
+
+/*
+* @Getter - só getters
+* @Setter - só setters
+* */
+
+@Data // Getters e Setters
 public class UserModel {
     private String username;
     private String name;
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
