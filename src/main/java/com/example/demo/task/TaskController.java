8b42ae3304023743cf7cf50e3d35c456343346a7
duduzx02 <duduzx02@gmail.com>
2023-10-14 03:01:31 -0300

    Validação dos horarios de começo e fim das tasks

--- src/main/java/com/example/demo/task/TaskController.java
+++ src/main/java/com/example/demo/task/TaskController.java
@@ -2,11 +2,14 @@ package com.example.demo.task;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -17,12 +20,27 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+
+
+
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
-        System.out.println("Chegou no controller");
+
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início / data de término deve ser " +
+                    "maior do que a data atual" +
+                    ".");
+        }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio dever ser menor do que a " +
+                    "data de termino.");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 
 }
