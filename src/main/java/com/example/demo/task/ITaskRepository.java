4302bab40751cc9c09f951f0075596dbe423f0d2
duduzx02 <duduzx02@gmail.com>
2023-10-14 00:00:47 -0300

    Cadastro de tarefas

---
+++ src/main/java/com/example/demo/task/ITaskRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+}
