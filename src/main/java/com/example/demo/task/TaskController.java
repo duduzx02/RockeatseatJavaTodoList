ebc387d183d913b16822ec1ec3b46632b28edc65
duduzx02 <duduzx02@gmail.com>
2023-10-15 21:24:31 -0300

    Update Parcial

--- src/main/java/com/example/demo/task/TaskController.java
+++ src/main/java/com/example/demo/task/TaskController.java
@@ -1,5 +1,6 @@
 package com.example.demo.task;
 
+import com.example.demo.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -48,10 +49,11 @@ public class TaskController {
 
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 
 }
